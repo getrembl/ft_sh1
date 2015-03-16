13e1a3ca1bd6405e074b0e26c2954fd70eb6304b
Durr-Tremblin <karakhirn@Mac-mint-17.1-cinnamon>
2015-03-16 14:22:14 +0100

    add env commit tite chatte

--- env_pwd.c
+++ env_pwd.c
@@ -1,23 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   env_usr.c                                          :+:      :+:    :+:   */
+/*   env_pwd.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: getrembl <getrembl@student.42.fr>          +#+  +:+       +#+        */
+/*   By: karakhirn <marvin@42.fr>                   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2015/03/13 15:28:12 by getrembl          #+#    #+#             */
-/*   Updated: 2015/03/14 20:03:13 by karakhirn        ###   ########.fr       */
+/*   Created: 2015/03/14 20:01:53 by karakhirn         #+#    #+#             */
+/*   Updated: 2015/03/16 00:08:08 by karakhirn        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell_1.h"
 
-char		*env_usr(char const *usr)
+char		*env_pwd(char const *pwd)
 {
 	char	*bkp;
 
-	bkp = usr;
-	bkp = ft_strchr(bkp, '=');
-	*bkp++;
-	return(bkp);
 }
